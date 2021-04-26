4ee2d26dfd625db123b2768da99b8bc576d7fee2
Nate Boldt <nate.h.boldt@gmail.com>
2021-04-26 18:19:21 -0500

    attempt at creating events from schedule

--- src/main/java/com/comp474/calender/comp474calender/Service/ScheduleService.java
+++ src/main/java/com/comp474/calender/comp474calender/Service/ScheduleService.java
@@ -2,11 +2,15 @@ package com.comp474.calender.comp474calender.Service;
 
 import com.comp474.calender.comp474calender.dao.ScheduleRepo;
 import com.comp474.calender.comp474calender.entity.*;
+import com.fasterxml.jackson.databind.util.JSONWrappedObject;
+import org.json.simple.*;
+import java.time.Month;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.comp474.calender.comp474calender.Service.EventService;
 
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
@@ -35,11 +39,26 @@ public class ScheduleService {
 
     private List<Events> generateEventsFromSchedule(Schedules schedule) {
         List<Events> newEvents = new LinkedList<Events>();
+        JSONObject json = new JSONObject();
 
-        String startTime = schedule.getStart();
-        String endTime = schedule.getEnd();
         String weekDays = schedule.getWeekDay();
         String months = schedule.getMonth();
+        int year = schedule.getYear();
+
+        String startTime = schedule.getStart();
+        LocalDateTime start = LocalDateTime.of(year, Month.APRIL, 28, Integer.parseInt(startTime) , 0);
+        json.put("start", start);
+
+        String endTime = schedule.getEnd();
+        LocalDateTime end = LocalDateTime.of(year, Month.APRIL, 28, Integer.parseInt(endTime) , 0);
+
+        json.put("end", end);
+
+        String emailId = schedule.getOwnerId();
+        json.put("users_emailid", emailId);
+
+//        Events newEvent = new Events(json);
+
 
         return newEvents;
     }
